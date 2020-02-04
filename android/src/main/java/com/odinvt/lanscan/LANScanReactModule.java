0e46fb83f1296b0488e02217675fd9c9f0733598
Gabriel Amorim <gabrielrra.dev@gmail.com>
2020-02-04 19:57:20 -0300

    removed @override

--- android/src/main/java/com/odinvt/lanscan/LANScanReactModule.java
+++ android/src/main/java/com/odinvt/lanscan/LANScanReactModule.java
@@ -11,11 +11,23 @@ import java.util.Collections;
 import java.util.List;
 
 public class LANScanReactModule implements ReactPackage {
+    @Override
+    public List<NativeModule> createNativeModules(ReactApplicationContext reactContext) {
+        return Arrays.<NativeModule>asList(new ScanlanModule(reactContext));
+    }
+
+    @Override
+    public List<ViewManager> createViewManagers(ReactApplicationContext reactContext) {
+        return Collections.emptyList();
+    }
+}
+
+public class LANScanReactModule implements ReactPackage {
 
     // @Override
-    // public List<Class<? extends JavaScriptModule>> createJSModules() {
-    //     return Collections.emptyList();
-    // }
+    public List<Class<? extends JavaScriptModule>> createJSModules() {
+        return Collections.emptyList();
+    }
 
     @Override
     public List<ViewManager> createViewManagers(ReactApplicationContext reactContext) {
