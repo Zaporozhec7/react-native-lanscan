30247529d4da55fb2ab9beeb667de60ef14d8f19
Gabriel Amorim <gabrielrra.dev@gmail.com>
2020-02-04 20:05:55 -0300

    UpdatedAgain

--- android/src/main/java/com/odinvt/lanscan/LANScanReactModule.java
+++ android/src/main/java/com/odinvt/lanscan/LANScanReactModule.java
@@ -1,16 +1,23 @@
-package com.reactlibrary;
+package com.odinvt.lanscan;
 
+import com.facebook.react.ReactPackage;
+import com.facebook.react.bridge.JavaScriptModule;
+import com.facebook.react.bridge.NativeModule;
+import com.facebook.react.bridge.ReactApplicationContext;
+import com.facebook.react.uimanager.ViewManager;
+
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
-import com.facebook.react.ReactPackage;
-import com.facebook.react.bridge.NativeModule;
-import com.facebook.react.bridge.ReactApplicationContext;
-import com.facebook.react.uimanager.ViewManager;
-import com.facebook.react.bridge.JavaScriptModule;
+public class LANScanReactModule implements ReactPackage {
 
-public class ScanlanPackage implements ReactPackage {
+    // @Override
+    public List<Class<? extends JavaScriptModule>> createJSModules() {
+        return Collections.emptyList();
+    }
+
     @Override
     public List<NativeModule> createNativeModules(ReactApplicationContext reactContext) {
         return Arrays.<NativeModule>asList(new LANScanModule(reactContext));
